e1b1d186c9b63d56006e28cea7b35b1d4687ca9a
Anton Prokhorov <betrayer11@gmail.com>
2017-01-22 00:07:05 +0700

    Add custom user admin class

---
+++ src/core/admin.py
@@ -0,0 +1,51 @@
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from core.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """A custom user admin class"""
+    filter_horizontal = ('groups', 'user_permissions')
+
+    readonly_fields = ('password', 'last_login', 'date_joined')
+
+    fieldsets = (
+        (
+            _('General information'), {
+                'fields': (
+                    'email',
+                    'username',
+                    'first_name',
+                    'last_name',
+                )
+            }
+        ),
+        (
+            _('Settings'), {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (
+            _('Permissions'), {
+                'fields': (
+                    'groups',
+                    'user_permissions',
+                )
+            }
+        ),
+        (
+            _('Additional information'), {
+                'fields': (
+                    'password',
+                    'last_login',
+                    'date_joined',
+                )
+            }
+        ),
+    )
