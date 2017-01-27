68a514262c14ac945151a9c46abe37bb79b72105
Anton Prokhorov <betrayer11@gmail.com>
2017-01-27 20:38:57 +0700

    Add some fields to project and task models

--- src/apps/projects/models.py
+++ src/apps/projects/models.py
@@ -1,17 +1,21 @@
 from django.db import models
-from django_extensions.db.models import TitleDescriptionModel
+from django_extensions.db.models import TimeStampedModel, TitleDescriptionModel
 
 
-class Project(TitleDescriptionModel):
+class Project(TitleDescriptionModel, TimeStampedModel):
     """Project model"""
     pass
 
 
-class Task(TitleDescriptionModel):
+class Task(TitleDescriptionModel, TimeStampedModel):
     """Task model
 
     Attributes:
         project (Project): project of card
+        completed (bool): completed flag
+        completed_datetime (datetime): time when task was completed
 
     """
     project = models.ForeignKey('Project')
+    completed = models.BooleanField()
+    completed_datetime = models.DateTimeField(blank=True, null=True)
